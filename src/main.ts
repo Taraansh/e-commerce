55e9e8d2a74e2bc5cfbf30fd7f74f871d6ad9f23
Taraansh <taraanshnandwani123@gmail.com>
2024-02-02 17:12:28 +0530

    Cookie Parser

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformationInterceptor } from './responseInterceptor';
+import cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
   app.useGlobalInterceptors(new TransformationInterceptor());
   await app.listen(3000);
 }
