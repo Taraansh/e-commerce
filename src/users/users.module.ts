1e8bbaf9707cb94aeeac9afd4b43edfd74482bb3
Taraansh <taraanshnandwani123@gmail.com>
2024-02-02 17:15:24 +0530

    Added Guard and middleware consumer

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UserRepository } from 'src/shared/repositories/user.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Users, UsersSchema } from 'src/shared/schema/users';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from 'src/shared/middleware/roles.guard';
+import { AuthMiddleware } from 'src/shared/middleware/auth';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UserRepository],
+  providers: [
+    UsersService,
+    UserRepository,
+    { provide: APP_GUARD, useClass: RolesGuard },
+  ],
   imports: [
     MongooseModule.forFeature([
       {
@@ -17,4 +29,10 @@ import { Users, UsersSchema } from 'src/shared/schema/users';
     ]),
   ],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes({ path: '/users', method: RequestMethod.GET });
+  }
+}
