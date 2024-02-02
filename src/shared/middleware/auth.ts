bd4720fb483a59cb0a303673c33e552c02d83378
Taraansh <taraanshnandwani123@gmail.com>
2024-02-02 17:12:45 +0530

    Auth middleware

---
+++ src/shared/middleware/auth.ts
@@ -0,0 +1,37 @@
+import {
+  ForbiddenException,
+    Inject,
+    Injectable,
+    NestMiddleware,
+    UnauthorizedException,
+  } from '@nestjs/common';
+  import { NextFunction, Request, Response } from 'express';
+  import { UserRepository } from '../repositories/user.repository';
+  import { decodeAuthToken } from '../utility/token-generator';
+  
+  @Injectable()
+  export class AuthMiddleware implements NestMiddleware {
+    constructor(
+      @Inject(UserRepository) private readonly userDB: UserRepository,
+    ) {}
+  
+    async use(req: Request | any, res: Response, next: NextFunction) {
+      try {
+        console.log('AuthMiddleware', req.headers);
+        const token = req.cookies.auth_token;
+        if (!token) {
+          throw new UnauthorizedException('Missing auth token');
+        }
+        const decodedData: any = decodeAuthToken(token);
+        const user = await this.userDB.findById(decodedData._id);
+        if (!user) {
+          throw new ForbiddenException('Unauthorized');
+        }
+        user.password = undefined;
+        req.user = user;
+        next();
+      } catch (error) {
+        throw new UnauthorizedException(error.message);
+      }
+    }
+  }
