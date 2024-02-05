f1da60c07e628d30034ed66a004ff7b2c6beb039
Taraansh <taraanshnandwani123@gmail.com>
2024-02-05 15:58:01 +0530

    Added appPrefix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformationInterceptor } from './responseInterceptor';
-import cookieParser = require('cookie-parser');
+import cookieParser from 'cookie-parser'
+import config from 'config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix(config.get('appPrefix'))
   app.use(cookieParser());
   app.useGlobalInterceptors(new TransformationInterceptor());
   await app.listen(3000);
