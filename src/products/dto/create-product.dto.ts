124ea2a3478c8270936b22fa7f954c9f02f100d2
Taraansh <taraanshnandwani123@gmail.com>
2024-02-05 16:00:13 +0530

    Create Product Dto

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,56 @@
+import { IsArray, IsEmpty, IsEnum, IsOptional, IsString } from 'class-validator';
+import { SkuDetails, baseType, catgeoryType, platformType } from 'src/shared/schema/products';
+
+export class CreateProductDto {
+  @IsString()
+  @IsEmpty()
+  productName: string;
+
+  @IsString()
+  @IsEmpty()
+  description: string;
+
+  @IsOptional()
+  image?: string;
+
+  @IsOptional()
+  imageDetails: Record<string, any>;
+
+  @IsString()
+  @IsEmpty()
+  @IsEnum(catgeoryType)
+  catgeory: string;
+
+  @IsString()
+  @IsEmpty()
+  @IsEnum(platformType)
+  platformType: string;
+
+  @IsString()
+  @IsEmpty()
+  @IsEnum(baseType)
+  baseType: string;
+
+  @IsString()
+  @IsEmpty()
+  productUrl: string;
+
+  @IsString()
+  @IsEmpty()
+  downloadUrl: string;
+
+  @IsArray()
+  @IsEmpty()
+  requirementSpecification: Record<string, any>[];
+
+  @IsArray()
+  @IsEmpty()
+  highlights: string[];
+
+  @IsOptional()
+  @IsArray()
+  skuDetails: SkuDetails[];
+
+  @IsOptional()
+  stripeProductId?: string;
+}
