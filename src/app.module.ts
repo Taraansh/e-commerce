7d172d43cc00e4a6b98b20cb84159899777d3b32
Taraansh <taraanshnandwani123@gmail.com>
2024-02-05 15:57:44 +0530

    Started ProductModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AllExceptionFilter } from './httpExceptionFilter';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/e-commerce'),
     UsersModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [
