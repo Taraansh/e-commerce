1492d4469fa1d1dc24fb803e70deb07f129402e4
Taraansh <taraanshnandwani123@gmail.com>
2024-02-05 15:58:27 +0530

    Product and other schemas

---
+++ src/shared/schema/products.ts
@@ -0,0 +1,122 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+export enum catgeoryType {
+  operatingSystem = 'Operating System',
+  applicationSoftware = 'Application Software',
+}
+
+export enum platformType {
+  windows = 'Windows',
+  mac = 'Mac',
+  linux = 'Linux',
+  android = 'Android',
+  ios = 'iOS',
+}
+
+export enum baseType {
+  computer = 'Computer',
+  mobile = 'mobile',
+}
+
+@Schema({ timestamps: true })
+export class Feedbackers extends mongoose.Document {
+  @Prop({})
+  customerId: string;
+
+  @Prop({})
+  customerName: string;
+
+  @Prop({})
+  rating: number;
+
+  @Prop({})
+  feedbackMessage: string;
+}
+
+export const FeedbackSchema = SchemaFactory.createForClass(Feedbackers);
+
+@Schema({ timestamps: true })
+export class SkuDetails extends mongoose.Document {
+  @Prop({})
+  skuName: string;
+
+  @Prop({})
+  price: number;
+
+  @Prop({})
+  validity: number; //in days
+
+  @Prop({})
+  lifetime: boolean;
+
+  @Prop({})
+  stripePriceId: string;
+
+  @Prop({})
+  skuCode?: string;
+}
+
+export const SkuDetailsSchema = SchemaFactory.createForClass(SkuDetails);
+
+@Schema({ timestamps: true })
+export class Products {
+  @Prop({ required: true })
+  productName: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ default: "https://st4.depositphotos.com/14953852/24787/v/450/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg" })
+  image?: string;
+
+  @Prop({
+    required: true,
+    enum: [catgeoryType.applicationSoftware, catgeoryType.operatingSystem],
+  })
+  category: string;
+
+  @Prop({
+    required: true,
+    enum: [
+      platformType.android,
+      platformType.ios,
+      platformType.linux,
+      platformType.mac,
+      platformType.windows,
+    ],
+  })
+  platformType: string;
+
+  @Prop({ required: true, enum: [baseType.computer, baseType.mobile] })
+  baseType: string;
+
+  @Prop({ required: true })
+  productUrl: string;
+
+  @Prop({ required: true })
+  downloadUrl: string;
+
+  @Prop({})
+  avgRating: number;
+
+  @Prop([{ type: FeedbackSchema }])
+  feedbackDetails: Feedbackers[];
+
+  @Prop([{ type: SkuDetailsSchema }])
+  skuDetails: SkuDetails[];
+
+  @Prop({ type: Object})
+  imageDetails: Record<string, any>;
+
+  @Prop({})
+  requirementSpecification: Record<string, any>[];
+
+  @Prop({})
+  highlights: string[];
+
+  @Prop({})
+  stripeProductId: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Products);
